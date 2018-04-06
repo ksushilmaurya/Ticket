9c272247bd064c73e5a6b75de562a7b4907ff1d2
dollysingh0010 <dollysingh0010@gmail.com>
2018-04-06 11:20:39 +0530

    register commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,36 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var MongoClient = require('mongodb').MongoClient,format=require('util').format;
+var url = 'mongodb://localhost:27017/masterDb';
+router.post('/registerUser',function(req,res,next){
+	console.log("User are going to register");
+	var name = req.body.name;
+	var email = req.body.email;
+	var phone = req.body.phone;
+	var company = req.body.company;
+
+	var item = {
+
+			name : name,
+			email : email,
+			phone : phone,
+			company :company
+	};
+
+	if(email)
+	{
+ 		MongoClient.connect(url,function(err,db){
+		 console.log("user going to insert - ",item);
+		 db.collection('User').insert(item,function(err,result){
+			console.log('insert done', result);
+			db.close();
+		});
+	});
+	} else 
+	{
+		return res.send({status: 200, msg: "Plase enter a email"})
+	}
+});
+
 module.exports = router;
